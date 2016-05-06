907a87e16466ed85f3ccacdb6ad78b5acbef6481
gadi gomez <gadi gomez>
2016-05-06 19:21:39 +0300

    added camera controls

--- 1/snake/snake.py
+++ 1/snake/snake.py
@@ -1,5 +1,13 @@
 import pygame, math, sys, random, world, pygame.gfxdraw, imutils
 from pygame.locals import *
+
+#import ocv stuff
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+
 WIDTH = 1024
 HEIGHT = 600
 BACK = (204, 255, 255)
@@ -19,33 +27,131 @@ pygame.mixer.init(44100, -16, 2, 2048)
 MUSIC = pygame.mixer.Sound(r'assets\banjo.ogg')
 EAT = pygame.mixer.Sound(r'assets\Eat.ogg')
 
+offset = (0,0)
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize the
+# list of tracked points
+LowerHsv = (151, 124, 107)
+UpperHsv = (194, 201, 201)
+LowerRgb = (0,22,146)
+UpperRgb = (124,136,255)
+
+camera = cv2.VideoCapture(0)
+
+def processCamera():
+	# grab the current frame
+	(grabbed, frame) = camera.read()
+	
+	# resize the frame, blur it, and convert it to the HSV
+	# color space
+	frame = imutils.resize(frame, width=WIDTH, height=HEIGHT)
+	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	rgb = frame.copy()
+	
+	# construct a mask for the color "green", then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+	mask = cv2.inRange(hsv, LowerHsv, UpperHsv)
+	mask = cv2.inRange(rgb, LowerRgb, UpperRgb)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = (0,0)
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		#center = (WIDTH - center[0], center[1])
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            	# update the points queue
+
+	# show the frame to our screen
+	#cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	return center
+
+def init():
+    done = False
+    global offset
+    
+    while not done:
+        screen.fill(BACK)
+        pygame.event.get()
+
+        pos = processCamera()
+        pos = (pos[0] + offset[0], pos[1] + offset[1])
+        pygame.draw.circle(screen, FOOD, (WIDTH / 2, HEIGHT / 2), 30, 1)
+        pygame.draw.circle(screen, FORE, pos, 10, 0)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+                    break # break out of the for loop
+                elif event.key == pygame.K_SPACE:
+                    offset = (WIDTH / 2 - pos[0], HEIGHT / 2 - pos[1])
+                    print offset
+                    break
+            elif event.type == pygame.QUIT:
+                done = True
+                break # break out of the for loop
+
+        if done:
+            break # to break out of the while loop		
+         
+        pygame.display.flip()
+        
+
 def mainGame():
+	done = False
 	my_world = world.world(30, make_food())
 	MUSIC.play()
-	while True:
-		#screen.fill(BACK)
+	while not done:
+
 		clock.tick(30)
 		screen.blit(BACK_IMG, (0, 0))
-		pygame.event.get()
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					done = True
+					break # break out of the for loop
+			elif event.type == pygame.QUIT:
+				done = True
+				break # break out of the for loop
+                
+                
+		if done:
+			break # to break out of the while loop		
+
 		my_world.positions.append(pygame.mouse.get_pos())
 		my_world.positions = my_world.positions[-my_world.length:]
-		#if (check_collision(my_world.positions)):
-		#	return
+
 		if (len(my_world.positions) > 1):
 			pygame.gfxdraw.filled_polygon(screen, create_poly(my_world.positions), FORE)
-			#pygame.gfxdraw.bezier(screen, create_poly(my_world.positions), 2, FORE)
-			#pygame.draw.polygon(screen, FORE, create_poly(my_world.positions), 0)
-			#pygame.draw.aalines(screen, FORE, True, create_poly(my_world.positions))
-		#else:
-			
-		#for i in range(len(my_world.positions)):
-			#p = my_world.positions[i]
-			#radius = int(HEAD_RADIUS * float(i) / my_world.length + TAIL_RADIUS * float(my_world.length - i) / my_world.length)
-			#pygame.draw.circle(screen, FORE, p, radius, 0)
+
 		handle_food(my_world)
 		if (my_world.length < my_world.new_length):
 			my_world.length += 1
-		#pygame.draw.circle(screen, FOOD, my_world.food, 8, 0)
+
 		screen.blit(BIRD_IMG, map(lambda n: n - FOOD_SIZE / 2, my_world.food))
 		pygame.display.flip()
 
@@ -61,7 +167,7 @@ def create_poly(positions):
 		alpha = math.atan2(positions[i + 1][1] - positions[i][1], positions[i + 1][0] - positions[i][0])
 		beta = alpha + math.pi / 2
 		gamma = alpha - math.pi / 2
-		#x, y = (positions[i][0] + positions[i + 1][0]) / 2, (positions[i][1] + positions[i + 1][0]) / 2
+
 		x, y = positions[i]
 		left = (x + math.cos(beta) * radius, y + math.sin(beta) * radius)
 		right = (x + math.cos(gamma) * radius, y + math.sin(gamma) * radius)
@@ -74,7 +180,7 @@ def create_poly(positions):
 	
 def handle_food(world):
 	if (dist(world.food, world.positions[len(world.positions) - 1]) < MIN_COLLIDE_RADIUS):
-		#world.new_length = int(world.length * GROWTH_RATE)
+
 		world.new_length += 15
 		world.food = make_food()
 		EAT.play()
@@ -98,6 +204,7 @@ def check_collision(positions):
 	return False
 		
 def main():
+	init()
 	mainGame()
 	
 	
